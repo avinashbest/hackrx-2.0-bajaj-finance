cb0edabce7ae225e9e1c39dd9fd07679d59aaf2b
Avinash Kumar <avinashbest3@gmail.com>
2021-07-08 15:19:00 +0530

    [KYCVerificationActivity]: User Interface Created

--- app/src/main/java/com/aviza/finserv/HomeActivity.kt
+++ app/src/main/java/com/aviza/finserv/HomeActivity.kt
@@ -1,11 +1,18 @@
 package com.aviza.finserv
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_home.*
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        kycButton.setOnClickListener {
+            val intent = Intent(this, KYCVerificationActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
---
+++ app/src/main/java/com/aviza/finserv/KYCVerificationActivity.kt
@@ -0,0 +1,11 @@
+package com.aviza.finserv
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class KYCVerificationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_kycverification)
+    }
+}
