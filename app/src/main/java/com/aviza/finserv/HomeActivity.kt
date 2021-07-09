ece64c52a7fd211f6b475b4668192bc4b8a9c3da
Avinash Kumar <avinashbest3@gmail.com>
2021-07-09 16:03:35 +0530

    [UI Improvement]: Making User Interface more Adaptive

--- app/src/main/java/com/aviza/finserv/HomeActivity.kt
+++ app/src/main/java/com/aviza/finserv/HomeActivity.kt
@@ -14,5 +14,60 @@ class HomeActivity : AppCompatActivity() {
             val intent = Intent(this, KYCVerificationActivity::class.java)
             startActivity(intent)
         }
+
+        payToMerchant.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+        transferMoney.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+        addRupee.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+        button1.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+        button2.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+        button3.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+        button4.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+        button5.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+        button6.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+        button7.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+        button8.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
