1ce3cfb3f53ea64f573f032a8792148833c6a2af
Avinash Kumar <avinashbest3@gmail.com>
2021-07-10 14:33:41 +0530

    [Firebase]: Current User Sign Out Connected to Profile Button

--- app/src/main/java/com/aviza/finserv/HomeActivity.kt
+++ app/src/main/java/com/aviza/finserv/HomeActivity.kt
@@ -3,12 +3,22 @@ package com.aviza.finserv
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_home.*
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        val auth = FirebaseAuth.getInstance()
+
+        profile.setOnClickListener {
+            auth.signOut()
+            val intent = Intent(this, LogInActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
 
         kycButton.setOnClickListener {
             val intent = Intent(this, KYCVerificationActivity::class.java)
